dbc86fbb3daed790f8525d1b553b313b62990b53
whdgusdl48 <52066712+whdgusdl48@users.noreply.github.com>
2020-01-27 20:00:42 +0900

    리액트 외부 API 연동 애플리케이션

---
+++ React API 연동/NewsList.jsx
@@ -0,0 +1,55 @@
+import React ,{useState,useEffect}from 'react';
+import styled from 'styled-components';
+import NewsItem from './NewsItem';
+import axios from 'axios';
+
+const NewsListBlock = styled.div`
+    box-sizing : border-box;
+    padding-bottom:3rem;
+    width:768px;
+    margin : 0 auto;
+    margin-top : 2rem;
+    @media screen and (max-width : 768px){
+        width : 100%;
+        padding-left : 1rem;
+        padding-right : 1rem;
+    }
+    `;
+
+
+const NewsList = ({category}) => {
+    const [articles,setArticles] = useState(null);
+    const [loading,setLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const query = category === 'all' ? '' : `&category=${category}`;
+                const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=ec5d9e337c8040f8a306c11791592485`,);
+                setArticles(response.data.articles);
+            }
+            catch(e){
+                console.log(e);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    },[category]);
+
+    if(loading){
+        return<NewsListBlock>대기 중......</NewsListBlock>
+    }
+    if(!articles){
+        return null
+    }
+    return(
+        <NewsListBlock>
+            {articles.map(article => (
+                <NewsItem key={article.url} article={article} />
+            ))}
+        </NewsListBlock>
+    )
+}
+
+export default NewsList;
